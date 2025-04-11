e068843c88a8117a91dc9eb612070ffe86b68aa4
Balázs Tárnok <tbalazs0205@gmail.com>
2025-04-11 22:54:52 +0200

    Add integration test

--- vars/runIntegrationTests.groovy
+++ vars/runIntegrationTests.groovy
@@ -1,8 +1,10 @@
 def call() {
     sh '''
+    bash -c "
     cd ${ROS_WS}
     source devel/setup.bash
     rostest my_package integration_test.launch
+    "
     '''
     junit '**/build/test_results/**/*.xml'
 }
