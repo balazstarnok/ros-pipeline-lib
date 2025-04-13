f19047aa22ce62bfb5d77eb380edb1adbd3e6def
Balázs Tárnok <tbalazs0205@gmail.com>
2025-04-13 16:55:09 +0200

    Add integration test

--- vars/runIntegrationTests.groovy
+++ vars/runIntegrationTests.groovy
@@ -1,10 +1,13 @@
 def call() {
     sh '''
     bash -c "
-    cd ${ROS_WS}
-    source devel/setup.bash
-    rostest gem_gazebo gem_simulation_integration.test
+        cd ${ROS_WS}
+        source devel/setup.bash
+        rostest gem_gazebo gem_simulation_integration.test || true
+        mkdir -p $WORKSPACE/test-artifacts/log
+        mkdir -p $WORKSPACE/test-artifacts/test_results
+        cp -r /root/.ros/log/* $WORKSPACE/test-artifacts/log/ || true
+        cp -r /root/.ros/test_results/* $WORKSPACE/test-artifacts/test_results/ || true
     "
     '''
-    junit '**/build/test_results/**/*.xml'
-}
+}    
