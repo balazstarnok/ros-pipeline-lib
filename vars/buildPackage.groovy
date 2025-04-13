997a43a9919ab3a9cff3b66b8997ef3b11d89a9b
Balázs Tárnok <tbalazs0205@gmail.com>
2025-04-13 21:50:54 +0200

    Add build package

--- vars/buildPackage.groovy
+++ vars/buildPackage.groovy
@@ -1,8 +1,13 @@
+// catkin init creates .catkin_tools/profiles/ if it doesn't exist
+// catkin config sets up the workspace correctly to find ROS packages (/opt/ros/noetic) and build in Release mode
 def call() {
     sh '''
-    cd ${ROS_WS}
-    source /opt/ros/noetic/setup.bash
-    catkin build --no-status
+    bash -c "
+        cd ${ROS_WS}
+        source /opt/ros/noetic/setup.bash
+        catkin init || true
+        catkin config --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE=Release
+        catkin build --no-status
     "
     '''
 }
