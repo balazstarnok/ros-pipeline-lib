9a3b9d80ecf702decf16d450c266f7c6e3f5e099
balazstarnok <tbalazs0205@gmail.com>
2025-04-17 18:24:40 +0200

    Update runUnitTests.groovy

--- vars/runUnitTests.groovy
+++ vars/runUnitTests.groovy
@@ -5,12 +5,12 @@ def call() {
     source devel/setup.bash
     catkin run_tests
     catkin_test_results || true
-    mkdir -p /root/gem_ws/test-results
+    mkdir -p $WORKSPACE/unit-tests-results
     # Copy gtest XML files
-    /usr/bin/find /root/gem_ws/build -type f -name 'gtest-*.xml' -exec cp {} /root/gem_ws/test-results/ \\;
+    /usr/bin/find /root/gem_ws/build -type f -name 'gtest-*.xml' -exec cp {} $WORKSPACE/unit-tests-results/ \\;
 
     # List copied test results
-    ls -l /root/gem_ws/test-results
+    ls -l $WORKSPACE/unit-tests-results/
     "
     '''
 }
