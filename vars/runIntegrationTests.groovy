053ec495c80688046545749ea470ef8a372a7045
balazstarnok <tbalazs0205@gmail.com>
2025-04-21 21:42:15 +0200

    Update runIntegrationTests.groovy

--- vars/runIntegrationTests.groovy
+++ vars/runIntegrationTests.groovy
@@ -8,6 +8,9 @@ def call() {
         mkdir -p $WORKSPACE/test-artifacts/test_results
         cp -r /root/.ros/log/* $WORKSPACE/test-artifacts/log/ || true
         cp -r /root/.ros/test_results/* $WORKSPACE/test-artifacts/test_results/ || true
+
+        # List copied test results
+        ls -l $WORKSPACE/test-artifacts/test_results/
     "
     '''
 }    
