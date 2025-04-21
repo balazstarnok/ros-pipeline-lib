13874c2af98ca59f1382bb278cf1eca82267cd5f
balazstarnok <tbalazs0205@gmail.com>
2025-04-21 19:05:13 +0200

    Update runUnitTests.groovy

--- vars/runUnitTests.groovy
+++ vars/runUnitTests.groovy
@@ -8,6 +8,7 @@ def call() {
     mkdir -p $WORKSPACE/test-artifacts/unit-tests-results
     # Copy unit test result XML files
     /usr/bin/find /root/gem_ws/build -type f -name 'gtest-*.xml' -exec cp {} $WORKSPACE/test-artifacts/unit-tests-results/ \\;
+    /usr/bin/find /root/gem_ws/build -type f -name 'nosetests-*.xml' -exec cp {} $WORKSPACE/test-artifacts/unit-tests-results/ \\;
 
     # List copied test results
     ls -l $WORKSPACE/test-artifacts/unit-tests-results/
